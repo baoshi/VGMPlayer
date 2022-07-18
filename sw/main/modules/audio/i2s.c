40cb1020ad43e5c3e03e9fc88cc4d1a0b703d3f2
baoshi <baoshi.zhu@gmail.com>
2022-07-18 12:59:03 +0800

    more comments

--- sw/main/modules/audio/i2s.c
+++ sw/main/modules/audio/i2s.c
@@ -18,7 +18,7 @@ extern uint32_t  tx_buf1[];
 extern uint32_t  tx_buf1_len;
 extern bool      cur_tx_buf; 
 
-// i2s program offset for cleanup
+// i2s PIO program offset for cleanup
 uint pio_i2s_offset = 0;
 
 // caller supplied callbacks
@@ -26,14 +26,35 @@ static i2s_notify_cb_t nofity_cb = 0;
 static void* notify_cb_param = 0;
 
 
+
+static void i2s_dma_channel_irq_enable()
+{
+#if (I2S_DMA_IRQ == DMA_IRQ_0)
+    dma_channel_set_irq0_enabled(DMA_CHANNEL_I2S_TX, true);
+#else
+    dma_channel_set_irq1_enabled(DMA_CHANNEL_I2S_TX, true);
+#endif    
+}
+
+
+static void i2s_dma_channel_irq_disable()
+{
+#if (I2S_DMA_IRQ == DMA_IRQ_0)
+    dma_channel_set_irq0_enabled(DMA_CHANNEL_I2S_TX, false);
+#else
+    dma_channel_set_irq1_enabled(DMA_CHANNEL_I2S_TX, false);
+#endif    
+}
+
+
 static void i2s_dma_isr()
 {
-    if (dma_hw->I2S_DMA_INTS & (1u << DMA_CHANNEL_I2S_TX))  // interript because I2S TX finish
+    if (dma_hw->I2S_DMA_INTS & (1u << DMA_CHANNEL_I2S_TX))  // interrupt because I2S TX finish
     {   
         dma_hw->I2S_DMA_INTS |= 1u << DMA_CHANNEL_I2S_TX;   // clear the interrupt request
         if (cur_tx_buf && tx_buf0_len > 0)
         {
-            // was transmitting buf1, and buf0 contains data
+            // transmit buf1 finished, buf0 contains new data
             dma_channel_transfer_from_buffer_now(DMA_CHANNEL_I2S_TX, tx_buf0, tx_buf0_len);
             cur_tx_buf = false;
             tx_buf1_len = 0;    // default length for next buffer
@@ -42,7 +63,7 @@ static void i2s_dma_isr()
         } 
         else if (!cur_tx_buf && tx_buf1_len > 0)
         {
-            // was transmiting buf0, and buf1 contains data
+            // transmit buf0 finished, buf1 contains new data
             dma_channel_transfer_from_buffer_now(DMA_CHANNEL_I2S_TX, tx_buf1, tx_buf1_len);
             cur_tx_buf = true;
             tx_buf0_len = 0;    // default length for next buffer
@@ -52,9 +73,9 @@ static void i2s_dma_isr()
         else
         {
             // No more data, no need DMA irq anymore
-            irq_set_enabled(I2S_DMA_IRQ, false);
+            i2s_dma_channel_irq_disable();
             // Notify playback finishing
-            // Not this is finishing instead of finished. There are still data in the FIFO. 
+            // Note this is finishing instead of finished. There are still data in the FIFO. 
             // Use i2s_stop_playback() to flush FIFO and finish playback.
             if (nofity_cb) nofity_cb(I2S_NOTIFY_PLAYBACK_FINISHING, notify_cb_param);
         }
@@ -64,7 +85,7 @@ static void i2s_dma_isr()
 
 static void pio_i2s_start()
 {   
-    // set init pin state, can only be done when statemachine is diabled
+    // set init pin state, can only be done when state machine is diabled
     pio_sm_set_enabled(I2S_PIO, I2S_PIO_SM, false);
     uint pin_mask = (1u << I2S_DATAOUT_PIN) | (3u << I2S_BCLK_PIN);
     pio_sm_set_pins_with_mask(I2S_PIO, I2S_PIO_SM, pin_mask, pin_mask); // set all 3 pins to 1 intially
@@ -74,13 +95,15 @@ static void pio_i2s_start()
     pio_sm_exec(I2S_PIO, I2S_PIO_SM, pio_encode_set(pio_y, 14));
     // immediate jump to entry point, statemachine not enabled yet
     pio_sm_exec(I2S_PIO, I2S_PIO_SM, pio_encode_jmp(pio_i2s_offset + pio_i2s_offset_entry_point));
-    // start statemachine
+    // start statem achine
     pio_sm_set_enabled(I2S_PIO, I2S_PIO_SM, true);
 }
 
 
 static void pio_i2s_flush()
 {
+    // No IRQ
+    i2s_dma_channel_irq_disable();
     // Let PIO execute until stalled
     pio_sm_set_enabled(I2S_PIO, I2S_PIO_SM, true);
     static uint32_t SM_STALL_MASK = 1u << (PIO_FDEBUG_TXSTALL_LSB + I2S_PIO_SM);
@@ -90,7 +113,7 @@ static void pio_i2s_flush()
     while (!(I2S_PIO->fdebug & SM_STALL_MASK))
     {
         tight_loop_contents();
-    };
+    }
 }
 
 
@@ -112,13 +135,12 @@ void i2s_init()
     sm_config_set_sideset_pins(&conf, I2S_BCLK_PIN);
     // Statemachine init
     pio_sm_init(I2S_PIO, I2S_PIO_SM, pio_i2s_offset, &conf);
-    
     // Setup I2S PIO clock
     uint32_t system_clock_frequency = clock_get_hz(clk_sys);
-    // 2 PIO cycles = 1 BCLK
+    // Each BCLK cycles contains 2 PIO instruction (see i2s.pio)
     // PIO_CLK = BCLK * 2
-    // BCLK = AUDIO_SAMPLE_RATE * 32
-    // PIO_CLK = AUDIO_SAMPLE_RATE * 64
+    //  -> BCLK = AUDIO_SAMPLE_RATE * 32
+    //  -> PIO_CLK = AUDIO_SAMPLE_RATE * 64
     // divider is fraction number in 16:8 fixed point format,
     // divider = 256 * SYS_CLK / PIO_CLK * 256 
     //         = 256 * SYS_CLK / (AUDIO_SAMPLE_RATE * 64)
@@ -126,7 +148,6 @@ void i2s_init()
     uint32_t divider = system_clock_frequency * 4 / AUDIO_SAMPLE_RATE;
     assert(divider < 0x1000000);
     pio_sm_set_clkdiv_int_frac(I2S_PIO, I2S_PIO_SM, divider >> 8u, divider & 0xffu);
-
     // Configure DMA channel to send audio samples to I2S
     dma_channel_claim(DMA_CHANNEL_I2S_TX);
     dma_channel_config i2s_tx_dma_cfg = dma_channel_get_default_config(DMA_CHANNEL_I2S_TX);
@@ -134,35 +155,28 @@ void i2s_init()
     channel_config_set_read_increment(&i2s_tx_dma_cfg, true);            // auto increase pointer
     channel_config_set_dreq(&i2s_tx_dma_cfg, pio_get_dreq(I2S_PIO, I2S_PIO_SM, true));  // I2S TX transfer request signal
     dma_channel_configure(
-        DMA_CHANNEL_I2S_TX, // DMA channel
+        DMA_CHANNEL_I2S_TX,         // DMA channel
         &i2s_tx_dma_cfg,
-        &I2S_PIO->txf[I2S_PIO_SM],   // Write address (only need to set this once)
-        NULL,               // Don't provide a read address yet
-        0,                  // Don't give transfer count yet
-        false               // Don't start yet
+        &I2S_PIO->txf[I2S_PIO_SM],  // Write address (only need to set this once)
+        NULL,                       // Don't provide a read address yet
+        0,                          // Don't give transfer count yet
+        false                       // Don't start yet
     );
     // Tell the DMA to raise IRQ when the channel finishes a block
-#if (I2S_DMA_IRQ == DMA_IRQ_0)
-    dma_channel_set_irq0_enabled(DMA_CHANNEL_I2S_TX, true);
-#else
-    dma_channel_set_irq1_enabled(DMA_CHANNEL_I2S_TX, true);
-#endif    
     irq_add_shared_handler(I2S_DMA_IRQ, i2s_dma_isr, PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
+    // It is possible I2S_DMA_IRQ and ST7789_DMA_IRQ are set to use same IRQ, just blindly enable it. Don't disable in the future
+    irq_set_enabled(I2S_DMA_IRQ, true);
 }
 
 
 void i2s_deinit()
 {
-    // disable and remove DMA irq
-    irq_set_enabled(I2S_DMA_IRQ, false);
+    // Disable DMA irq
+    i2s_dma_channel_irq_disable();
+    // remove IRQ handler (internally will disable IRQ before removing then restore)
     irq_remove_handler(I2S_DMA_IRQ, i2s_dma_isr);
     // let DMA finish
     dma_channel_wait_for_finish_blocking(DMA_CHANNEL_I2S_TX);
-#if (I2S_DMA_IRQ == DMA_IRQ_0)
-    dma_channel_set_irq0_enabled(DMA_CHANNEL_I2S_TX, false);
-#else
-    dma_channel_set_irq1_enabled(DMA_CHANNEL_I2S_TX, false);
-#endif
     dma_channel_unclaim(DMA_CHANNEL_I2S_TX);
     // cleanup PIO    
     pio_sm_set_enabled(I2S_PIO, I2S_PIO_SM, false);
@@ -174,7 +188,7 @@ void i2s_deinit()
 
 void i2s_send_buffer_blocking(uint32_t *buf, uint32_t len)
 {
-    irq_set_enabled(I2S_DMA_IRQ, false);
+    i2s_dma_channel_irq_disable();
     pio_i2s_start();
     for (uint32_t i = 0; i < len; ++i)
     {
@@ -189,7 +203,7 @@ void i2s_start_playback(i2s_notify_cb_t notify, void *param)
 {
     nofity_cb = notify;
     notify_cb_param = param;
-    irq_set_enabled(I2S_DMA_IRQ, true);
+    i2s_dma_channel_irq_enable();
     pio_i2s_start();
     if (cur_tx_buf)
     {
@@ -204,7 +218,6 @@ void i2s_start_playback(i2s_notify_cb_t notify, void *param)
 
 void i2s_stop_playback()
 {
-    irq_set_enabled(I2S_DMA_IRQ, false);
     dma_channel_abort(DMA_CHANNEL_I2S_TX);
 #if (I2S_DMA_IRQ == DMA_IRQ_0)
     dma_channel_acknowledge_irq0(DMA_CHANNEL_I2S_TX);
--- sw/main/modules/audio/i2s.h
+++ sw/main/modules/audio/i2s.h
@@ -5,20 +5,21 @@ extern "C" {
 #endif
 
 
+
+
 enum 
 {
     I2S_NOTIFY_SAMPLE_REQUESTED = 1,
     I2S_NOTIFY_PLAYBACK_FINISHING = 2
 };
-
-
-typedef void (*i2s_notify_cb_t)(int notify, void *);
+typedef void (*i2s_notify_cb_t)(int notification, void *);
 
 void i2s_init();
 void i2s_deinit();
-void i2s_send_buffer_blocking(uint32_t *buf, uint32_t len);
-void i2s_start_playback(i2s_notify_cb_t notify, void *param);
+void i2s_start_playback(i2s_notify_cb_t callback, void *param);
 void i2s_stop_playback();
+
+void i2s_send_buffer_blocking(uint32_t *buf, uint32_t len);
 
 
 #ifdef __cplusplus
