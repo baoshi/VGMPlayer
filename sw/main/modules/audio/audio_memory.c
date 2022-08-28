dcbdff5fbf6614f448d6a0c42a9db025724d0fa4
baoshi <baoshi.zhu@gmail.com>
2022-08-28 11:41:34 +0800

    Increase audio frame length

--- sw/main/modules/audio/audio_memory.c
+++ sw/main/modules/audio/audio_memory.c
@@ -4,7 +4,7 @@
 
 // "Heap" used by audio function in core1mem region
 
-#define AUDIO_HEAP_SIZE 0xBA00
+#define AUDIO_HEAP_SIZE 0x9F00
 static uint8_t __audio_ram("heap") audio_heap[AUDIO_HEAP_SIZE];
 
 
